c7db1e61c8c38cbc9e06793f69daecd2ecd7fe8b
Joshua-Simon <joshy.d.simon@gmail.com>
2020-04-24 23:07:05 +1000

    finished

--- schoolCatalogue.js
+++ schoolCatalogue.js
@@ -25,3 +25,33 @@ class School {
     typeof newNumberOfStudents === 'number' ? this._numberOfStudents = newNumberOfStudents : console.log('Invalid input: numberOfStudents must be set to a Number.')
   }
 }
+
+class PrimarySchool extends School {
+  constructor(name,numberOfStudents, pickupPolicy) {
+    super(name,'primary',numberOfStudents)
+    this._pickupPolicy = pickupPolicy
+  }
+  get pickupPolicy () {
+    return this._pickupPolicy
+  }
+}
+
+class HighSchool extends School {
+  constructor(name,numberOfStudents,sportsTeams) {
+    super(name,'High School', numberOfStudents)
+    this._sportsTeams = sportsTeams
+  }
+  get sportsTeams () {
+    return this._sportsTeams
+  }
+}
+
+const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.' )
+
+lorraineHansbury.quickFacts()
+
+School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli'])
+
+const alSmith = new HighSchool('Al E. Smith', 415,['Baseball', 'Basketball', 'Volleyball', 'Track and Field'] )
+
+console.log(alSmith.sportsTeams)
