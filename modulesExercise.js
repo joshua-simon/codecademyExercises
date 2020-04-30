38d75eab4e97a13ab0a54edb41014e845b797b64
Joshua-Simon <joshy.d.simon@gmail.com>
2020-04-30 13:38:42 +1000

    module stuff

---
+++ modulesExercise.js
@@ -0,0 +1,22 @@
+let availableAirplanes = [
+    {
+     name: 'AeroJet',
+     fuelCapacity: 800,
+     availableStaff: ['pilots','flightAttendants','engineers',         'medicalAssistance', 'sensorOperators']
+     },
+    {
+      name: 'SkyJet',
+      fuelCapacity: 500,
+      availableStaff: ['pilots','flightAttendants']
+      }
+    ]
+  
+  let flightRequirements = {
+    requiredStaff: 4,
+  };
+  
+  const meetsStaffRequirements = (availableStaff, requiredStaff) => {
+    return availableStaff.length >= requiredStaff ? true : false;
+  }
+  
+  export {availableAirplanes, flightRequirements, meetsStaffRequirements};
