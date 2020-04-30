ef3936ff283f6676ff18b02c8d4211eabdef365f
Joshua-Simon <joshy.d.simon@gmail.com>
2020-05-01 01:26:14 +1000

    fizzbuzz

--- randomAlgorithms.js
+++ randomAlgorithms.js
@@ -26,4 +26,20 @@ const reverse = (string) => {
     return newString
   }
 
+//fizzbuzz
 
+const fizzBuzz = () => {
+  for (let i=0; i <=100; i++) {
+    if (i%3===0 && i%5===0){
+      console.log('FizzBuzz')
+    } else if (i%5===0){
+      console.log('Buzz')
+    } else if (i%3===0) {
+      console.log('Fizz')
+    } else {
+      console.log(i)
+    }
+  }
+}
+
+fizzBuzz()
